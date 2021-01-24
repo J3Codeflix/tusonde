3c91b390589bddf9da8c2fc3eaf7710a8e78c5ef
seiji-lash <seiji.lash@gmail.com>
2021-01-24 18:55:46 +0800

    Add Settings page

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserProfile } from '../@core/models/auth/user.model';
+import { AuthService } from '../@core/services/auth/auth.service';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  currentUser: UserProfile = null;
 
-  constructor() {}
+  constructor(private authService: AuthService) {
+    (async () => {
+      try {
+        this.currentUser = await this.authService.getUserProfile();
+      } catch (error) {
+        console.error('🚀 ~ file: tab3.page.ts ~ line 18 ~ Tab3Page ~ error', error);
+      }
+    })();
+  }
 
 }
